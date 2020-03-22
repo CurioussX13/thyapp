9d744f99ae19d341a1fb754e13198834286036a6
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2020-03-22 14:20:39 +0530

    Create MainActivity.java

---
+++ src/main/java/com/thyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.main);
+    }
+}
