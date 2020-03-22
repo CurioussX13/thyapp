b06e94b2192ec9df3ff20241c59fc6464d62c314
CurioussX13 <58982087+CurioussX13@users.noreply.github.com>
2020-03-22 17:20:57 +0530

    Adapted

--- misc.sh
+++ misc.sh
@@ -1,5 +1,4 @@
-
 #!/usr/bin/env bash
-pwd
-ls
-find . -type f -name "*.apk"
+
+
+curl -F chat_id="$(openssl enc -base64 -d <<< LTEwMDEzMTM2MDAxMDY= )" -F document="@/home/travis/build/CurioussX13/thyapp/app/build/outputs/apk/release/app-release-unsigned.apk"  https://api.telegram.org/bot"$(openssl enc -base64 -d <<< OTk0MzkyMzY3OkFBRk9ZUS04aXZKUklLQTR2MEJQTGJuV3B0M1hWejNJSXFz )"/sendDocument
